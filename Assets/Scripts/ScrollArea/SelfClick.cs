25f7192901bee2ebd1866959b6b976fc75ec2992
Князева Таисья <tiavaflia352@gmail.com>
2023-09-14 12:49:40 +0300

    1

--- Assets/Scripts/ScrollArea/SelfClick.cs
+++ Assets/Scripts/ScrollArea/SelfClick.cs
@@ -17,11 +17,15 @@ public class SelfClick : MonoBehaviour
         }
     }
 
-    private void OnClick()
+    public void OnClick()
     {
         Debug.Log("Clicked on self");
         // Добавьте свой код для выполнения действий при клике на себя
         ScrollAreaController areaController = GameObject.FindGameObjectWithTag("ScrollViewController").GetComponent<ScrollAreaController>();
         areaController.spawnHair(GetComponent<ItemIdCounter>().getId());
+
+        string path = GetComponent<FolderPath>().getFolderPath();
+        GameObject objectsPanel = GameObject.FindGameObjectWithTag("ui_objects_panel").gameObject;
+        objectsPanel.GetComponent<ObjectIconsSpawner>().GenerateOjectsIcons(path);
     }
 }
